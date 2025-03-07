b6d6fe8fcb4b36877f9915079b22607a919b3794
Sung Jun Cho <chosung0531@naver.com>
2025-03-07 21:27:04 +0900

    Function : Revise Auto Build
    - Auto Build 코드 수정
    - Project Setting 변경
    - 스크립트 폴더 위치 변경

--- Assets/Editor/AutoBuild.cs
+++ Assets/Editor/AutoBuild.cs
@@ -9,62 +9,66 @@ public class AutoBuild
 {
     static string[] SCENES = FindEnableEditorScenes();
     static string TARGET_DIR = "Build";
-    static string APP_NAME = "kks";
+    const string APP_NAME = "AshenVanguard";
     //static int BUILD_DATE = 0;
 
+    /// <summary>
+    /// Get Scene Names
+    /// </summary>
+    /// <returns></returns>
     static string[] FindEnableEditorScenes()
     {
         List<string> EditorScenes = new List<string>();
-
-        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        EditorBuildSettingsScene[] sceneSet = EditorBuildSettings.scenes;
+        int sceneCnt = sceneSet.Length;
+        for(int i=0; i<sceneCnt; i++)
         {
-            if (!scene.enabled)
+            if (!sceneSet[i].enabled)
                 continue;
-
-            EditorScenes.Add(scene.path);
+            EditorScenes.Add(sceneSet[i].path);
         }
-
+        //foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        //{
+        //    if (!scene.enabled)
+        //        continue;
+        //    EditorScenes.Add(scene.path);
+        //}
         return EditorScenes.ToArray();
     }
 
+    #region Editor
     [MenuItem("Custom/Version/CodeUp", false, 1)]
     static void CodeUp() 
     {
         int code = PlayerSettings.Android.bundleVersionCode;
-
         code += 1;
-
         PlayerSettings.Android.bundleVersionCode = code;
-
-        Debug.Log(" ===== Code ===== " + code );
+        //Debug.Log(" ===== Code ===== " + code );
     }
 
     [MenuItem("Custom/Build/Android")]
     static void AndroidBuild()
     {
         string BUILD_TARGET_PATH = TARGET_DIR + "/Android/";
-        
         Directory.CreateDirectory(BUILD_TARGET_PATH);
 
-        PlayerSettings.companyName = "ksgames";
-        PlayerSettings.productName = "kks";
+        PlayerSettings.companyName = "JunPortfolio";
+        PlayerSettings.productName = "AshenVanguard";
 
         PlayerSettings.Android.keystoreName = Application.dataPath + "/user.keystore";
-        PlayerSettings.Android.keystorePass = "kkskks";
-        PlayerSettings.Android.keyaliasName = "kks";
-        PlayerSettings.Android.keyaliasPass = "kkskks";
-
+        PlayerSettings.Android.keystorePass = "junStorePass";
+        PlayerSettings.Android.keyaliasName = "junAliasName";
+        PlayerSettings.Android.keyaliasPass = "junAliasPass";
         PlayerSettings.bundleVersion = Application.version;
 
         string FileName = APP_NAME + ".apk";
-
         GenericBuild(SCENES, BUILD_TARGET_PATH + FileName, BuildTarget.Android, BuildOptions.None);
     }
 
     static void GenericBuild(string[] scenes, string filename, BuildTarget buildtarget, BuildOptions buildoption)
     {
         //EditorUserBuildSettings.SwitchActiveBuildTarget(buildtarget);
-
         BuildPipeline.BuildPlayer(scenes, filename, buildtarget, buildoption);
     }
+    #endregion
 }
